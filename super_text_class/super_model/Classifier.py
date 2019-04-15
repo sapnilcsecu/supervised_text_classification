c6ea1d1ba7449542f8c9e381877a7bd0995a9925
rmisys123 <sapnil123@yahoo.com>
2019-04-16 02:19:07 +0600

    add the native bayes classifier

--- super_text_class/super_model/Classifier.py
+++ super_text_class/super_model/Classifier.py
@@ -29,44 +29,21 @@ def load_dataset():
     return trainDF
 # load the dataset
 
-# split the dataset into training and validation datasets 
-trainDF=load_dataset()
-train_x, valid_x, train_y, valid_y = model_selection.train_test_split(trainDF['text'], trainDF['label'])
-# split the dataset into training and validation datasets 
-
-# label encode the target variable 
-encoder = preprocessing.LabelEncoder()
-train_y = encoder.fit_transform(train_y)
-valid_y = encoder.fit_transform(valid_y)
-# label encode the target variable 
-
-# word level tf-idf
-tfidf_vect = TfidfVectorizer(analyzer='word', token_pattern=r'\w{1,}', max_features=5000)
-tfidf_vect.fit(trainDF['text'])
-xtrain_tfidf =  tfidf_vect.transform(train_x)
-xvalid_tfidf =  tfidf_vect.transform(valid_x)
-# word level tf-idf
-# ngram level tf-idf 
-tfidf_vect_ngram = TfidfVectorizer(analyzer='word', token_pattern=r'\w{1,}', ngram_range=(2,3), max_features=5000)
-tfidf_vect_ngram.fit(trainDF['text'])
-xtrain_tfidf_ngram =  tfidf_vect_ngram.transform(train_x)
-xvalid_tfidf_ngram =  tfidf_vect_ngram.transform(valid_x)
-# ngram level tf-idf 
-
-# characters level tf-idf
-tfidf_vect_ngram_chars = TfidfVectorizer(analyzer='char', token_pattern=r'\w{1,}', ngram_range=(2,3), max_features=5000)
-tfidf_vect_ngram_chars.fit(trainDF['text'])
-xtrain_tfidf_ngram_chars =  tfidf_vect_ngram_chars.transform(train_x) 
-xvalid_tfidf_ngram_chars =  tfidf_vect_ngram_chars.transform(valid_x) 
-# characters level tf-idf
+def train_model(classifier, feature_vector_train, label, feature_vector_valid, is_neural_net=False,valid_y):
+    # fit the training dataset on the classifier
+    classifier.fit(feature_vector_train, label)
     
-# create a count vectorizer object 
-count_vect = CountVectorizer(analyzer='word', token_pattern=r'\w{1,}')
-count_vect.fit(trainDF['text'])
+    # predict the labels on validation dataset
+    predictions = classifier.predict(feature_vector_valid)
+    
+    if is_neural_net:
+        predictions = predictions.argmax(axis=-1)
+    
+    return metrics.accuracy_score(predictions, valid_y)
 
-# transform the training and validation data using count vectorizer object
-xtrain_count =  count_vect.transform(train_x)
-xvalid_count =  count_vect.transform(valid_x)   
+
+"""    
+
 
 
 
@@ -96,17 +73,7 @@ for word, i in word_index.items():
 
 
 
-def train_model(classifier, feature_vector_train, label, feature_vector_valid, is_neural_net=False):
-    # fit the training dataset on the classifier
-    classifier.fit(feature_vector_train, label)
-    
-    # predict the labels on validation dataset
-    predictions = classifier.predict(feature_vector_valid)
-    
-    if is_neural_net:
-        predictions = predictions.argmax(axis=-1)
-    
-    return metrics.accuracy_score(predictions, valid_y)
+
 
 
 # Naive Bayes on Count Vectors
@@ -341,3 +308,4 @@ accuracy = train_model(classifier, train_seq_x, train_y, valid_seq_x, is_neural_
 print ("CNN, Word Embeddings",  accuracy)
 
 #Recurrent Convolutional Neural Network
+"""
