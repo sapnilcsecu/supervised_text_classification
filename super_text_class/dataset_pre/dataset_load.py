b3d9e9c1f08b95231f97957fef3217be434e3009
rmisys123 <sapnil123@yahoo.com>
2019-05-08 01:44:14 +0600

    remove load_dataset with .txt dataset

--- super_text_class/dataset_pre/dataset_load.py
+++ super_text_class/dataset_pre/dataset_load.py
@@ -13,30 +13,15 @@ class dataset_load:
     classdocs
     '''
 
-# load the dataset  
- 
-    def load_txt_dataset(self): 
-        data = open("../corpus.txt",encoding="utf8").read()
-        labels, texts = [], []
-        for i, line in enumerate(data.split("\n")):
-            content = line.split()
-            labels.append(content[0])
-            texts.append(" ".join(content[1:]))
-        
-        # create a dataframe using texts and lables
-        trainDF = pandas.DataFrame()
-        trainDF['text'] = texts
-        trainDF['label'] = labels
-        return trainDF
-    # load the dataset
+
     
     
-    def load_cvs_dataset(self,dataset_path):
+    def load_cvs_dataset(self,dataset_path,encoder):
         #Set Random seed
         np.random.seed(500)
         
         # Add the Data using pandas
-        Corpus = pd.read_csv(dataset_path,encoding='latin-1')
+        Corpus = pd.read_csv(dataset_path,encoding=encoder)
         #Corpus = pd.read_csv(dataset_path)
         
         return Corpus
